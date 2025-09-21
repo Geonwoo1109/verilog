6f9cc441a8fffce6e4ec2bd32499f02842be969e
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2025-09-22 04:24:51 +0900

    Create problem_06_TflipflopOneShot.v

---
+++ week_04/problem_06_TflipflopOneShot.v
@@ -0,0 +1,21 @@
+module Tflipflop_oneShot(clk, rst, T, Q);
+    input wire clk, rst, T;
+    output reg Q;
+    
+    reg T_reg;
+    
+    always @(posedge clk) begin
+        if (rst) begin
+            Q <= 0;
+            T_reg <= 0;
+        end
+        else begin
+            T_reg <= T;
+            if (T & ~T_reg) Q <= ~Q;
+        end
+    end
+    
+    
+            
+    
+endmodule
