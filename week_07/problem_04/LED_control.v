b66a17f1471c0e02eac0cceb0f5aeda33f7470b3
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2025-10-14 19:55:34 +0900

    Create LED_control.v

---
+++ week_07/problem_04/LED_control.v
@@ -0,0 +1,20 @@
+module LED_control(clk, rst, bin, cnt, seg_data, seg_sel, led_signal);
+    input clk, rst;
+    input [7:0] bin;
+    
+    output wire [7:0] cnt;
+    
+    output [7:0] seg_data, seg_sel;
+    output reg led_signal;
+    
+    counter8 c1(clk, rst, cnt);
+    seg7_controller s1(clk, rst, bin, seg_data, seg_sel);
+    
+    always @(posedge clk or posedge rst) begin
+        if (rst) led_signal <= 0;
+        else begin
+            if (cnt <= bin) led_signal <= 1;
+            else led_signal <= 0;
+        end
+    end
+endmodule
