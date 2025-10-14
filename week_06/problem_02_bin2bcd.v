0d81ac9d3329ab61fe76ff22a7bcb789ed3e1d1f
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2025-10-14 16:43:28 +0900

    Update problem_02_bin2bcd.v

--- week_06/problem_02_bin2bcd.v
+++ week_06/problem_02_bin2bcd.v
@@ -17,12 +17,12 @@ module bin2bcd(clk, rst, bin, bcd);
                 7: bcd <= {4'd0, 4'd7};
                 8: bcd <= {4'd0, 4'd8};
                 9: bcd <= {4'd0, 4'd9};
-                10: bcd <= {4'd1, 4'd10};
-                11: bcd <= {4'd1, 4'd11};
-                12: bcd <= {4'd1, 4'd12};
-                13: bcd <= {4'd1, 4'd13};
-                14: bcd <= {4'd1, 4'd14};
-                15: bcd <= {4'd1, 4'd15};
+                10: bcd <= {4'd1, 4'd0};
+                11: bcd <= {4'd1, 4'd1};
+                12: bcd <= {4'd1, 4'd2};
+                13: bcd <= {4'd1, 4'd3};
+                14: bcd <= {4'd1, 4'd4};
+                15: bcd <= {4'd1, 4'd5};
                 default: bcd <= {4'd0, 4'd0};
             endcase
         end
