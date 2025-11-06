7723fee2eda1d76c14ba4d25c8a35285d73f155c
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2025-11-07 06:36:31 +0900

    Create sub_bin2bcd.v

---
+++ week_10/problem_02/sub_bin2bcd.v
@@ -0,0 +1,39 @@
+// sub module
+
+module bin2bcd(clk, rst, bin, bcd_out);
+    input clk, rst;
+    input [7:0] bin;    // input: 8-bit binary number data
+    
+    reg [2:0] i;
+    reg [11:0] bcd;
+    
+    output reg [11:0] bcd_out;      // output: 3 of decimal digits (000 ~ 999)
+                                    // we need 0(0V) ~ 255(5V)
+    
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            bcd <= {4'd0, 4'd0, 4'd0};
+            i <= 0;
+        end
+        else begin
+            if (i == 0) begin
+                bcd[11:1] <= 11'b0000_0000_000;
+                bcd[0] <= bin[7];
+            end
+            else begin
+                bcd[11:9] <= (bcd[11:8] >= 3'd5) ? bcd[11:8] + 2'd3 : bcd[11:8];
+                bcd[8:5] <= (bcd[7:4] >= 3'd5) ? bcd[7:4] + 2'd3 : bcd[7:4];
+                bcd[4:1] <= (bcd[3:0] >= 3'd5) ? bcd[3:0] + 2'd3 : bcd[3:0];
+                bcd[0] <= bin[7-i];
+            end
+            i <= i + 1;
+        end
+    end
+    
+    always @(posedge clk or posedge rst) begin
+        if (rst) bcd_out <= {4'd0, 4'd0, 4'd0};
+        else begin
+            if (i == 0) bcd_out <= bcd;
+        end
+    end
+endmodule
