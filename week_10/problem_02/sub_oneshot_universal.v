7aab3eeb77c50ae260ef3ed378af21bc2e52c41f
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2025-11-07 06:35:58 +0900

    Create sub_oneshot_universal.v

---
+++ week_10/problem_02/sub_oneshot_universal.v
@@ -0,0 +1,21 @@
+// sub module
+
+module oneshot_universal(clk, rst, btn, btn_trig);    
+    input clk, rst;
+    
+    parameter WIDTH = 6;
+    input [WIDTH-1:0] btn;
+    reg [WIDTH-1:0] btn_reg;
+    output reg [WIDTH-1:0] btn_trig;
+    
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            btn_reg <= {WIDTH{1'b0}};
+            btn_trig <= {WIDTH{1'b0}};
+        end
+        else begin
+            btn_reg <= btn;
+            btn_trig <= btn & ~btn_reg;
+        end
+    end
+endmodule
